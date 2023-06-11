508ccf48646f2057e6a51f482b87f5486f1cc2da
alidaoudi1 <ali.daoudi@hotmail.fr>
2023-06-12 00:53:13 +0200

    cleaned css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
-const header = () => {
+import CTA from './CTA'
+import ME from '../../assets/me-about.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
   return (
-    <div>header</div>
-  )
+    <header>
+        <div className="container header__container">
+            <h5>Hello I'm</h5>
+            <h1>Ali</h1>
+            <h5 className="text-light">Fullstack Developer</h5>       
+            <CTA />
+            <HeaderSocials />
+            <div className='me'>
+                <img src={ME} alt=""/>
+            </div>
+            <a href='#contact' className='scroll_down'>Scroll Down</a>
+        </div>
+    </header> 
+ )
 }
 
-export default header
+export default Header
